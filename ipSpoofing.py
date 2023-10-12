d2b7642247c42f0a3f858fd7bb5371edf29fa13c
Inesh Tandon <i08polymath@gmail.com>
2023-10-12 17:57:13 +0530

    Updates - changes in commands which run on kali linux - hard coding the interface 'eth0'

--- ipSpoofing.py
+++ ipSpoofing.py
@@ -1,22 +1,22 @@
 import subprocess
 import re
 
-def get_current_ip_address(iface):
+def get_current_ip_address():
 
-    output = subprocess.check_output(f"ifconfig {iface}", shell=True).decode()
+    output = subprocess.check_output(f"ifconfig eth0", shell=True).decode()
     return re.search("inet (.+) ", output).group().split()[1].strip()
 
 def get_router_ip_address():
 
-        output = subprocess.check_output(f"netstat -nr | grep default",shell = True).decode()
-        return re.search("default (.+) ",output).group().split()[1].strip()
+        output = subprocess.check_output(f"ip r | grep default",shell = True).decode()
+        return re.search("via (.+) ",output).group().split()[1].strip()
 
 def change_ip_address(routerAddr):
     
-    subprocess.check_output(f"ifconfig en0 {routerAddr}", shell=True)
+    subprocess.check_output(f"ifconfig eth0 {routerAddr}", shell=True)
 
 if __name__ == "__main__":
-      curIp = get_current_ip_address("en0")
+      curIp = get_current_ip_address()
       curRouterIp = get_router_ip_address()
       print("current ip address is = ",curIp)
       change_ip_address(curRouterIp)
